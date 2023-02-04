af52cde0823da319dd7e27afc75b0ddc7b15ad80
K Shreevathsa <srivathsa.kalyan@gmail.com>
2023-02-04 21:24:42 +0530

    Programs on Java Data Types and Operators

---
+++ Test.java
@@ -0,0 +1,12 @@
+class Test {
+    // This program is for testing doubtful codes
+    public static void main(String args[]) {
+        /*System.out.println("hello" + '\t' + "there");
+        System.out.println('\b' - '\t');*/
+
+        System.out.println(1|4);
+        byte b = (byte) 0xf1;
+        System.out.println(b);
+        System.out.println(0xff); 
+    }
+}
