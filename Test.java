698afc2f772416629a00119b88d4766990a0bf8f
K Shreevathsa <srivathsa.kalyan@gmail.com>
2023-02-05 21:29:21 +0530

    modified test sample

--- Test.java
+++ Test.java
@@ -1,12 +1,19 @@
 class Test {
     // This program is for testing doubtful codes
     public static void main(String args[]) {
-        /*System.out.println("hello" + '\t' + "there");
-        System.out.println('\b' - '\t');*/
+        /*
+         * System.out.println("hello" + '\t' + "there");
+         * System.out.println('\b' - '\t');
+         */
 
-        System.out.println(1|4);
-        byte b = (byte) 0xf1;
-        System.out.println(b);
-        System.out.println(0xff); 
+        /*
+         * System.out.println(1|4);
+         * byte b = (byte) 0xf1;
+         * System.out.println(b);
+         * System.out.println(0xff);
+         */
+        int a = 3, b = 2;
+
+        System.out.println(a / b);
     }
 }
