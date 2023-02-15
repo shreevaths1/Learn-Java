74b542b80987ecbdf194b6df2acbd5808c9f721f
K Shreevathsa <srivathsa.kalyan@gmail.com>
2023-02-15 11:11:19 +0530

    added files

--- src/Test1.java
+++
@@ -1,8 +0,0 @@
-class Test1{
-	public static void main(String args[]) {
-		String s1 = "Hello.",s2="Jolly Launcher.",s3="Nexa";
-		System.out.println("s1.length(): "+s1.length());
-		System.out.println("s2.equals(\"Hello\"): "+s2.equals("Hello"));
-		System.out.println("s3.charAt(3): "+s3.charAt(3));
-	}
-}
