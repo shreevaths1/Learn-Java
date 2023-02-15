9a1ac185234f5e336fa1ddad1b88162735e8c23f
K Shreevathsa <srivathsa.kalyan@gmail.com>
2023-02-15 11:06:50 +0530

    variable arguments.

--- src/VarArgs.java
+++ src/VarArgs.java
@@ -1,4 +1,12 @@
 class VarArgs {
+
+	public static void main(String args[]) {
+		VarArgsTest.vaTest(1, 3, 5, 7); // implicitly creates an array called v of type int[];
+		VarArgsTest.vaTest();
+	}
+}
+
+class VarArgsTest{
 	static void vaTest(int... v) {
 		System.out.println("No. of elements: " + v.length);
 		System.out.print("Contents: ");
@@ -6,10 +14,5 @@ class VarArgs {
 			System.out.print(x + " ");
 		}
 		System.out.println("\n");
-	}
-
-	public static void main(String args[]) {
-		vaTest(1, 3, 5, 7); // implicitly creates an array called v of type int[];
-		vaTest();
 	}
 }
