266a41993a18535a34a9e05e44dfaa1ef237a845
K Shreevathsa <srivathsa.kalyan@gmail.com>
2023-02-23 21:57:46 +0530

    classes with same name in different packages.

---
+++ src/k1/Oppo.java
@@ -0,0 +1,13 @@
+package k1;
+
+public class Oppo {
+	public void disp() {
+		System.out.println("This is Oppo class in package k1.");
+	}
+}
+
+class Trial{
+	public static void main(String args[]) {
+		
+	}
+}
