861b13a79c86df83aef0dfea4c85620a8f258e7e
K Shreevathsa <srivathsa.kalyan@gmail.com>
2023-02-24 17:22:31 +0530

    using inheritance on interfaces.

---
+++ src/IFExtend.java
@@ -0,0 +1,29 @@
+interface A3{
+	void meth1();
+	void meth2();
+}
+
+interface B3 extends A3{
+	void meth3();
+}
+
+class MyClass implements B3{
+	public void meth1() {
+		System.out.println("Inside meth1().");
+	}
+	public void meth2() {
+		System.out.println("Inside meth2().");
+	}
+	public void meth3() {
+		System.out.println("Inside meth3().");
+	}
+}
+
+class IFExtend {
+	public static void main(String args[]) {
+		MyClass c = new MyClass();
+		c.meth1();
+		c.meth2();
+		c.meth3();
+	}
+}
