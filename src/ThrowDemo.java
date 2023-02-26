ea5614defee1b30b12fccbe317a6fa1f9e132d93
K Shreevathsa <srivathsa.kalyan@gmail.com>
2023-02-26 09:13:14 +0530

    throw Keyword.

---
+++ src/ThrowDemo.java
@@ -0,0 +1,20 @@
+import java.lang.Object;
+
+class ThrowDemo {
+	static void demoproc() {
+		try {
+			throw new NullPointerException("demo");
+		} catch (NullPointerException e) {
+			System.out.println("caught inside demoproc method.");
+			throw e;
+		}
+	}
+
+	public static void main(String args[]) {
+		try {
+			demoproc();
+		} catch (NullPointerException e) {
+			System.out.println("Recaught: " + e);
+		}
+	}
+}
